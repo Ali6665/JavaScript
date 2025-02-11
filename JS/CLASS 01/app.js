bdd87d24b0ca49dc72a6a6ded60b62d829be09f0
Muhammad Ali <bdon77150@gmail.com>
2025-02-12 03:11:44 +0900

    Add files via upload

---
+++ JS/CLASS 01/app.js
@@ -0,0 +1,73 @@
+// Chapter NO.1 ("Alert")
+
+// Q.01
+
+alert(" First Name \n \n Last Name \n \n Email \n \n Phone Number V Passward")
+
+// Q.02
+
+alert("Youre learning JavaScript! \n    Yes I am learning JS")
+
+// Q.03
+
+alert("When it comes out well, it's been done successfully.")
+
+
+// Chapter No.02 ("Variables for Strings")
+
+// Q.01
+
+var id = " My name is"     
+
+var name = " Muhammad Ali"
+var add = id + name
+alert(add)
+
+// Q.02
+// no answer 
+
+
+
+// Q.03
+// no answer 
+
+
+
+// Chapter No.03 ("Variables for numbers")
+
+// Q.01
+// no answer 
+
+
+// Q.02
+
+var a = 72;
+var b = 72;
+var c = a + b;
+alert(c);
+
+
+// Q.03
+
+var a = 3;
+var b = 3;
+var c = a * b;
+alert(c);
+
+// Q.04
+var a = 2;
+var b = 2;
+var c = a + b;
+alert(c);
+
+
+
+// Q.05
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal)
+
+// Q.06
+// no answer
